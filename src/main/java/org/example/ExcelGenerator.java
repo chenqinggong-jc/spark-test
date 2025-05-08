ba2bc78e8d605dea0848e34688182f40ba5d9ea2
chenqinggong <9566377+chenqinggong1@user.noreply.gitee.com>
2025-05-08 16:22:36 +0800

    main修改

--- src/main/java/org/example/ExcelGenerator.java
+++ src/main/java/org/example/ExcelGenerator.java
@@ -95,6 +95,7 @@ public class ExcelGenerator {
                 System.out.println("数据输出3 :"+data);
                 System.out.println("数据输出4 :"+data);
                 System.out.println("数据输出5 :"+data);
+                System.out.println("数据输出6 main修改 :"+data);
                 metaRow5.createCell(1).setCellValue(column_name_value); // 假设 column_name 是分区
                 metaRow5.getCell(0).setCellStyle(metaStyle);
                 metaRow5.getCell(1).setCellStyle(metaStyle);
